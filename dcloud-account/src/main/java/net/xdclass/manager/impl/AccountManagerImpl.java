ea36e56aef103cde1e41c1549fcffb3e67234dbe
rao2701482 <243954391@qq.com>
2022-10-20 18:04:03 +0800

    account服务可启动

---
+++ dcloud-account/src/main/java/net/xdclass/manager/impl/AccountManagerImpl.java
@@ -0,0 +1,20 @@
+package net.xdclass.manager.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import net.xdclass.manager.AccountManager;
+import org.springframework.stereotype.Component;
+
+/**
+ * 小滴课堂,愿景：让技术不再难学
+ *
+ * @Description
+ * @Author 二当家小D
+ * @Remark 有问题直接联系我，源码-笔记-技术交流群
+ * @Version 1.0
+ **/
+
+@Component
+@Slf4j
+public class AccountManagerImpl implements AccountManager {
+
+}
