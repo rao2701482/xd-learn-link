1844557f74a948fca7fcf90e1277d94f4c2b6219
rao2701482 <243954391@qq.com>
2022-11-04 21:13:33 +0800

    上传用户头像

---
+++ dcloud-account/src/main/java/net/xdclass/config/OSSConfig.java
@@ -0,0 +1,17 @@
+package net.xdclass.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "aliyun.oss")
+public class OSSConfig {
+
+    private String endpoint;
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String bucketname;
+
+}
--- dcloud-account/src/main/java/net/xdclass/controller/AccountController.java
+++ dcloud-account/src/main/java/net/xdclass/controller/AccountController.java
@@ -1,8 +1,15 @@
 package net.xdclass.controller;
 
 
+import net.xdclass.enums.BizCodeEnum;
+import net.xdclass.service.FileService;
+import net.xdclass.util.JsonData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * <p>
@@ -13,10 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-11-09
  */
 @RestController
-@RequestMapping("/api/v1/account")
+@RequestMapping("/api/account/v1")
 public class AccountController {
 
+    @Autowired
+    private FileService fileService;
 
+    /**
+     * 文件上传 最大默认1M
+     *  文件格式、拓展名等判断
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    public JsonData uploadUserImg(@RequestPart("file") MultipartFile file){
+
+        String result = fileService.uploadUserImg(file);
+
+        return result !=null ? JsonData.buildSuccess(result):JsonData.buildResult(BizCodeEnum.FILE_UPLOAD_USER_IMG_FAIL);
+
+    }
 
 }
 
