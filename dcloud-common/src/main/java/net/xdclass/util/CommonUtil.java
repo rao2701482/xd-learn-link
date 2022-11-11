9bda84fa1fba7cd967ac8d0d0d85dfbbd686d5d1
rao2701482 <243954391@qq.com>
2022-11-11 21:29:16 +0800

    生成短链码完毕

--- dcloud-common/src/main/java/net/xdclass/util/CommonUtil.java
+++ dcloud-common/src/main/java/net/xdclass/util/CommonUtil.java
@@ -1,5 +1,6 @@
 package net.xdclass.util;
 
+import com.google.common.hash.Hashing;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.http.HttpServletRequest;
@@ -185,6 +186,17 @@ public class CommonUtil {
         }
 
 
+    }
+
+
+    /**
+     * murmurhash算法
+     * @param param
+     * @return
+     */
+    public static long murmurHash32(String param){
+        long murmurHash32 = Hashing.murmur3_32().hashUnencodedChars(param).padToLong();
+        return murmurHash32;
     }
 
 }
