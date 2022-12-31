d1be9ecee9c7bb79d03c2d160985ede794bb4a86
rao2701482 <243954391@qq.com>
2022-12-31 17:22:14 +0800

    dwd层新老访客统计

---
+++ dcloud-app/src/main/java/net/xdclass/util/DeviceUtil.java
@@ -0,0 +1,51 @@
+package net.xdclass.util;
+
+import java.security.MessageDigest;
+import java.util.Map;
+
+/**
+ * 小滴课堂,愿景：让技术不再难学
+ *
+ * @Description
+ * @Author 二当家小D
+ * @Remark 有问题直接联系我，源码-笔记-技术交流群
+ * @Version 1.0
+ **/
+
+public class DeviceUtil {
+
+    /**
+     * 生成web设备唯一ID
+     * @param map
+     * @return
+     */
+    public static String geneWebUniqueDeviceId(Map<String,String> map){
+        String deviceId = MD5(map.toString());
+        return deviceId;
+    }
+
+
+
+    /**
+     * MD5加密
+     *
+     * @param data
+     * @return
+     */
+    public static String MD5(String data) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(data.getBytes("UTF-8"));
+            StringBuilder sb = new StringBuilder();
+            for (byte item : array) {
+                sb.append(Integer.toHexString((item & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString().toUpperCase();
+        } catch (Exception exception) {
+        }
+        return null;
+
+    }
+
+
+}
